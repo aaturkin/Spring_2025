62405627f4f901e72fa3ad72604ed9ff4b27df34
aaturkin <game.nano@yandex.ru>
2025-06-08 01:59:44 +0300

    Lr1

---
+++ Turkin_A_A_24VMz_Lr1/Код лабораторной работы.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Ввод слова A
+        Console.Write("Введите слово A: ");
+        string wordA = Console.ReadLine();
+
+        // Ввод слова B
+        Console.Write("Введите слово B: ");
+        string wordB = Console.ReadLine();
+
+        // Инициализируем пустую строку для результата
+        string result = "";
+
+        // Для каждой буквы в слове A
+        foreach (char letter in wordA)
+        {
+            // Если буква не в слове B
+            if (!wordB.Contains(letter))
+            {
+                // Добавляем букву к результату
+                result += letter;
+            }
+        }
+
+        // Выводим результат
+        Console.WriteLine($"Результат: {result}");
+    }
+}
